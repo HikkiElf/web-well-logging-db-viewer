663f86d662e0eb130ede337db6d4aa5d3aad13e5
sevaK5 <43607822+sevaK5@users.noreply.github.com>
2023-03-28 14:31:11 +0700

    early mockup of app

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,61 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <p>hello</p>
+  <main>
+    <header id="top">
+      <h1>"Project name" : "area name"</h1>
+    </header>
+    <div id="bottom">
+      <section class="left section">
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+      </section>
+      <section class="right section">
+        <h1>TABLE</h1>
+      </section>
+    </div>
+  </main>
+  
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+main{
+  margin: 0 auto;
+}
+
+header {
+  position: relative;
+  background-color: black;
+  border: 5px solid var(--test-border-color);
+  text-align: center;
+}
+h1 {
+  font-size: 28px;
+}
+#bottom {
+  display: flex;
+  border: 5px solid var(--test-border-color);
+}
+
+.right.section{
+  position: absolute;
+  border: 5px solid red;
+  right: 5px;
+}
+
+.left.section {
+  display: flex;
+  flex-direction: column;
+  border: 5px solid greenyellow;
+}
 
 </style>
