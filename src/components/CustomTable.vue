ce1166254c5cb68a7576ebc38dc1a27ebc860fe3
sevaK5 <43607822+sevaK5@users.noreply.github.com>
2023-03-28 21:08:00 +0700

    add CustomButton and CustomTable

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    props: {
+        data: Array,
+        columns: Array
+    },
+    data() {
+        return {
+            sortKey: '',
+            sortOrders: this.columns.reduce((o, key) => ((o[key] = 1), o), {})
+        }
+    },
+    methods: {
+        sortBy(key) {
+            this.sortKey = key
+            this.sortOrders[key] = this.sortOrders[key] * -1
+        }
+    }
+}
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>X</th>
+                <th>Y</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="nums in 100">
+                <td>{{ nums }}</td>
+                <td>{{ nums }}</td>
+                <td>{{ nums }}</td>
+            </tr>
+        </tbody>
+        
+    </table>
+
+</template>
